51d18528799f132272966d9cdce8f9ab8c9ee69c
Mohd Amir <Amirkhan98375@gmail.com>
2022-05-30 00:34:41 +0530

    backend complete

--- backend/PostJobs/index.js
+++ backend/PostJobs/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const connect=require('./src/config/db.js')
+const PostController=require('./src/controller/jobController.js')
+const JobPost=require('./src/model/jobpost.js')
 const app=express()
 app.use(express.json())
+app.use("/postjobs",PostController)
 
 app.listen(4000,()=>{
+    connect()
     console.log("listening on port 4000")
 })
